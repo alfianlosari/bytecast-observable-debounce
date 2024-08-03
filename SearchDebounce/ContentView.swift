00bc7056bfc9542b3be61e2bf46c573cd9583039
Alfian Losari <alfianlosari@gmail.com>
2024-08-03 22:04:34 +0700

    Initial Commit

---
+++ SearchDebounce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchDebounce
+//
+//  Created by Alfian Losari on 03/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
