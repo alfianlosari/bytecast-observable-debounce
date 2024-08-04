2455b454e4cf48e0b5b421e8efd776fd5a4a0935
Alfian Losari <alfianlosari@gmail.com>
2024-08-04 10:19:29 +0700

    Initial Commit

--- SearchDebounce/ContentView.swift
+++ SearchDebounce/ContentView.swift
@@ -1,24 +1,39 @@
 //
 //  ContentView.swift
-//  SearchDebounce
+//  ObservableXCombine
 //
-//  Created by Alfian Losari on 03/08/24.
+//  Created by Alfian Losari on 30/07/24.
 //
 
 import SwiftUI
 
+@MainActor
 struct ContentView: View {
+    
+    @State var vm = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(vm.state.data ?? [], id: \.self) {
+            Text($0)
         }
-        .padding()
+        .overlay {
+            if vm.state.isLoading {
+                ProgressView("Searching \"\(vm.searchText)\"")
+            }
+            
+            if vm.isSearchNotFound {
+                Text("Results not found for\n\"\(vm.searchText)\"")
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .searchable(text: $vm.searchText, prompt: "Search")
+        .navigationTitle("Observable X Combine Demo")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
+
